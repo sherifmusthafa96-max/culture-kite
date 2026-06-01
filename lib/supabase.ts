85f650291f8b187300c5661c6a17360589086cc7
Aiswarya <sherifmusthaf96@gmail.com>
2026-06-01 18:17:19 +0530

    Fixed Supabase and apply page

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
+console.log("SUPABASE URL:", supabaseUrl);
+
+export const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+);

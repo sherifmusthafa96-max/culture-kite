748cb037e8b25c7098389f95d265086062e64a61
Aiswarya <sherifmusthaf96@gmail.com>
2026-06-01 18:49:40 +0530

    Fix Zoho SMTP app password update

--- app/api/apply/route.ts
+++ app/api/apply/route.ts
@@ -23,7 +23,9 @@ export async function POST(req: Request) {
                 pass: process.env.EMAIL_PASS,
             },
         });
-
+        console.log("EMAIL_USER:", process.env.EMAIL_USER);
+        console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+        console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
         await transporter.verify();
         console.log("SMTP VERIFIED");
         console.log("API HIT");
